254b49a6a087e364a0f626e6e58c923ce80963da
shayan <shayansabahi94@gmail.com>
2017-11-28 16:20:04 +0330

    debug

--- shadabSuperVisorBot.py
+++ shadabSuperVisorBot.py
@@ -321,6 +321,21 @@ def isBotAdmin(update):
     else:
         return True
 
+def status(bot , update):
+    # if not is_admin(update):
+    #     return
+    members = Member.objects.filter(add_count__gt=0)
+    result = ''
+    for member in members:
+        result =  result + '<a href="tg://user?id='+str(member.t_id)+'">'+'adsd'+'  </a><b>'+str(member.add_count)+'</b>\n'
+    print(result)
+    bot.send_message(chat_id=431282203,
+                     text=result)
+    bot.send_message(chat_id=431282203,
+                         text=result,
+                         parse_mode='HTML')
+
+
 
 def shadab(bot,
            update  # type: telegram.Update
@@ -343,8 +358,8 @@ def shadab(bot,
 # dispatcher.add_handler(handler4, 3)
 handler1 = MessageHandler(Filters.all, all_message)
 dispatcher.add_handler(handler1, 4)
-# handler5 = CommandHandler('ajab', ajab)
-# dispatcher.add_handler(handler5, 4)
+handler5 = CommandHandler('status', status)
+dispatcher.add_handler(handler5, 5)
 # handler6 = CommandHandler('test', test, pass_args=True)
 # dispatcher.add_handler(handler6, 5)
 # handler7 = CommandHandler('start', onStart)
