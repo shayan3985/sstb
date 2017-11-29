0bcb27e3882d190034347cd170334620daeb2af2
shayan <shayansabahi94@gmail.com>
2017-11-29 21:58:59 +0330

    debug

--- database/admin.py
+++ database/admin.py
@@ -2,7 +2,46 @@ from django.contrib import admin
 from .models import Member , Spam , AddLog , BotAdmin
 # Register your models here.
 
-admin.site.register(Member)
+class CustomerMember(admin.ModelAdmin):
+    # The forms to add and change user instances
+    # form = CustomerChangeForm
+    # add_form = CustomerCreationForm
+
+    # def get_form(self, request, obj=None, **kwargs):
+    #     # Proper kwargs are form, fields, exclude, formfield_callback
+    #     if obj:
+    #         self.form = CustomerChangeForm
+    #     else:
+    #         self.form = CustomerCreationForm
+    #     return super(CustomerAdmin, self).get_form(request, obj, **kwargs)
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    model = Member
+    # list_display = ( 'username','first_name', 'last_name','first_time',)
+    # list_filter = ('last_name',)
+    # fieldsets = (
+    #     (None, {'fields': ('username', 'password')}),
+    #     ('Personal info', {'fields': ('first_name','last_name','phone'
+    #                                   ,)}),
+    #     ('Permissions', {'fields': ('is_admin','special_user','is_active',)}),
+    #
+    # )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    # add_fieldsets = (
+    #     (None, {
+    #         'classes': ('wide',),
+    #         'fields': ('username', 'first_name','last_name','phone', 'password1', 'password2')}
+    #     ),
+    #
+    # )
+
+    search_fields = ('t_id',)
+    filter_horizontal = ()
+
+admin.site.register(Member,CustomerMember)
 admin.site.register(Spam)
 admin.site.register(AddLog)
 admin.site.register(BotAdmin)
