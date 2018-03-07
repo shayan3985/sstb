9b9dd26b51af367cfc47fa9610cf2f720bc1b686
shayan <shayansabahi94@gmail.com>
2018-03-08 00:31:41 +0330

    ic

--- shadabsupervisor/settings.py
+++ shadabsupervisor/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@n945+gy_lx+je-(6mp*(18(fwv0j+yvb3+q)x@7c2pe2ytf^j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['148.251.25.153' ,'127.0.0.1',]
+ALLOWED_HOSTS = ['88.99.79.110' ,'127.0.0.1',]
 
 
 # Application definition
